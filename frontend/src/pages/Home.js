7a0e0b789fa4aaf5124b276ca3935daf32347aec
Anuja Sunil Kokate <anujakokate18@gmail.com>
2023-11-11 05:47:18 +0530

    First commit

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,11 @@
+import PageContent from '../components/PageContent';
+
+function HomePage() {
+  return (
+    <PageContent title="Welcome to EveReminder!">
+      <p>Browse all our amazing events!</p>
+    </PageContent>
+  );
+}
+
+export default HomePage;
